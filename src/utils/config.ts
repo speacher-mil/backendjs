96c2b1ec268adf3fb552781e4f4ec41d922f06c8
citrusinesis <jihojiho2003@gmail.com>
2024-08-24 14:49:22 +0900

    feat: config utils

---
+++ src/utils/config.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+function getEnvironmentFilePath(): string {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+    case 'development':
+      return '.env';
+    case 'local':
+      return '.env.local';
+    default:
+      return '.env';
+  }
+}
+
+dotenv.config({
+  path: path.resolve(getEnvironmentFilePath()),
+});
+
+function required<T>(key: string, defaultValue = ''): T {
+  if (!process.env[key] && typeof defaultValue === 'undefined') {
+    throw new Error('Missing required environment variable: ' + key);
+  }
+  return (process.env[key] as T) || (defaultValue as T);
+}
+
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+export const IS_TEST = process.env.NODE_ENV === 'test';
+export const IS_LOCAL = process.env.NODE_ENV
+  ? process.env.NODE_ENV.toString().startsWith('local')
+  : false;
+
+export const config = {
+  NODE_ENV: required<string>('NODE_ENV'),
+  JWT_SECRET: required<string>('JWT_SECRET'),
+};
