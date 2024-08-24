80dae8a387e03708144107e6e8704d692df5a17e
citrusinesis <jihojiho2003@gmail.com>
2024-08-24 15:36:55 +0900

    feat: add api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 import { AppModule } from '@/AppModule';
-import { config } from '@/utils/config';
+import { IS_PRODUCTION, config } from '@/utils/config';
+
+const setupSwaggerDocument = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setVersion('1.0.0')
+    .addTag('Misc', 'Miscellaneous APIs')
+    .build();
+
+  const options: SwaggerCustomOptions = {};
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, options);
+};
 
 (async () => {
   const app = await NestFactory.create(AppModule);
+  !IS_PRODUCTION && setupSwaggerDocument(app);
+
   await app.listen(config.PORT);
 })();
