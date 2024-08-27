45618b81ecc31a81a18af0a422cd10ce97dcb630
citrusinesis <jihojiho2003@gmail.com>
2024-08-27 18:38:30 +0900

    chore: tag `AppController` endpoints as misc

--- src/AppController.ts
+++ src/AppController.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiProduces, ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Misc')
 export class AppController {
   constructor() {}
 
   @Get()
+  @Header('Content-Type', 'text/plain; charset=utf-8')
+  @ApiOperation({ summary: 'Health check' })
+  @ApiOkResponse({ description: 'Success', type: String, example: 'ok' })
+  @ApiProduces('text/plain')
   healthCheck(): string {
     return 'ok';
   }
