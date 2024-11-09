ef58646d3f430d75833c9d8b869d936628173d4c
Jiho Song <jihojiho2003@gmail.com>
2024-11-09 22:39:51 +0900

    test: drizzle module creation test

---
+++ src/shared/persistance/drizzle/mysql/drizzle.mysql.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { DrizzleMySqlService } from './drizzle.mysql.service';
+
+describe('DrizzleMySqlService', () => {
+  let service: DrizzleMySqlService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [DrizzleMySqlService],
+    }).compile();
+
+    service = module.get<DrizzleMySqlService>(DrizzleMySqlService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ src/shared/persistance/drizzle/sqlite/drizzle.sqlite.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { DrizzleSqliteService } from './drizzle.sqlite.service';
+
+describe('DrizzleSqliteService', () => {
+  let service: DrizzleSqliteService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [DrizzleSqliteService],
+    }).compile();
+
+    service = module.get<DrizzleSqliteService>(DrizzleSqliteService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
